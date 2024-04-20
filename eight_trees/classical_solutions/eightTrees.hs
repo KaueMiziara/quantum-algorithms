08deef9cc59d2718fc944c39fe21e05ac2a932ca
Kaue Miziara <kauemiziara@gmail.com>
2024-04-20 00:17:34 -0300

    Rename function to countSets

--- eight_trees/classical_solutions/eightTrees.hs
+++ eight_trees/classical_solutions/eightTrees.hs
@@ -1,18 +1,18 @@
-countSubsets :: [Int] -> Int
-countSubsets [] = 0
-countSubsets xs =
+countSets :: [Int] -> Int
+countSets [] = 0
+countSets xs =
     if odd count then 1 else 0 where
-        count = countSubsets' xs 0 
-        countSubsets' :: [Int] -> Int -> Int
-        countSubsets' [] count = count
-        countSubsets' (1 : rest) count = 
-            countSubsets' (dropWhile (== 1) rest) (count + 1)
-        countSubsets' (_ : rest) count = 
-            countSubsets' rest count
+        count = countSets' xs 0 
+        countSets' :: [Int] -> Int -> Int
+        countSets' [] count = count
+        countSets' (1 : rest) count = 
+            countSets' (dropWhile (== 1) rest) (count + 1)
+        countSets' (_ : rest) count = 
+            countSets' rest count
 
 main :: IO ()
 main = do
     let inputList = [1, 0, 1, 1, 0, 1, 0, 1]
     putStrLn $ "Input: " ++ show inputList
-    putStrLn $ "Result: " ++ show (countSubsets inputList)
+    putStrLn $ "Result: " ++ show (countSets inputList)
 
